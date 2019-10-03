258e0c96b86db190afbe6bfd5f041efe140f7965
Miles A Kershner <mkershn16@ccs.local>
2019-10-03 09:29:26 -0400

    add fancyLEDMain.py

---
+++ CircuitPython/fancyLEDMain.py
@@ -0,0 +1,20 @@
+from fancyLED import FancyLED  #pylint: disable=import-error
+import board  #pylint: disable=import-error
+import time #pylint: disable=import-error
+
+fancy1 = FancyLED(board.D2,board.D3,board.D4)
+fancy2 = FancyLED(board.D5,board.D6,board.D7)
+
+while True:
+    print("working")
+    for i in range(0, 3):
+        fancy1.alternate()
+        time.sleep(1)
+    for i in range(0,3):
+        fancy2.blink()
+        time.sleep(1)
+    for i in range(0,3):
+        fancy1.chase()
+    for i in range(0, 15):
+        fancy2.sparkle()
+        
